3718207719afd83d84d39f1ab6f44f737babfe49
Amritha <amrithaaaa5@gmail.com>
2020-03-28 16:46:31 +0530

    Updated Scanner

--- include/scanner.hpp
+++ include/scanner.hpp
@@ -1,66 +1,75 @@
 #pragma once
 #include <string_view>
 
-namespace Quar {
+namespace quar {
+	enum class TokenType : uint8_t {
+	// Single-character tokens.                         
+  		TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,                
+  		TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,                
+  		TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,    
+  		TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
 
-enum class TokenType : uint8_t {
-	//Single-character tokens.
-	LeftParent, RightParent, LeftBrace, RightBrace,
-	Comma, Dot, Minus, Plus,
-	Semicolon, Slash, Star,
+  // One or two character tokens.                     
+  		TOKEN_BANG, TOKEN_BANG_EQUAL,                       
+ 	 	TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,                     
+  		TOKEN_GREATER, TOKEN_GREATER_EQUAL,                 
+  		TOKEN_LESS, TOKEN_LESS_EQUAL,                       
 
-	// One or two character tokens.
-	Bang, BangEqual, Equal, EqualEqual,
-	Greater, GreaterEqual, Less, LessEqual,
+  // Literals.                                        
+  		TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,       
 
-	// Literals.
-	Identifier, String, Number,
+  // Keywords.                                        
+  		TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,    
+  		TOKEN_FOR, TOKEN_FUN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
+  		TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS, 
+  		TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,                 
 
-	// Keywords.
-	And, Class, Else, False,
-	Fun, For, If, Nil,
-	Or, Print, Return, Super,
-	This, True, Var, While,
+  		TOKEN_ERROR,                                        
+  		TOKEN_EOF
+	};
 
-	Error, Eof,
-};
+	struct Token {
+		TokenType type;
+		std::string_view start;
+		size_t line = 1;
 
-struct Token {
-	TokenType type;
-	std::string_view start;
-	size_t line = 1;
+		Token(TokenType type, std::string_view start, size_t line) {
+			this->type = type;
+			this->start = start;
+			this->line = line;
+		}
+		Token() : type(TokenType::TOKEN_EOF) {
+		}
+		void print();
+	};
 
-	constexpr Token(TokenType type, std::string_view start, size_t line) : type(type), text(text), line(line) {
-	}
-	constexpr Token() : type(TokenType::Eof) {};
-};
+	struct Scanner {
+		size_t start = 0;
+		size_t current = 0;
+		size_t line = 1;
+		std::string_view source;
 
-struct Scanner {
-	size_t start = 0;
-	size_t current = 0;
-	size_t line = 1;
-	std::string_view source;
+		Scanner(std::string_view source) 
+			:source(source) {
+		}
 
-	constexpr Scanner(std::string_view source) 
-		:source(source) {
-	}
-
-	Token scan_token();
+		Token scanToken();
 	
-private :
-	char advance();
-	Token identifier();
-	Token number();
-	Token string();
-	TokenType identifierType()const;
-	TokenType check_keyword(size_t begin, size_t length, std::string_view rest, TokenType type)const;
-	Token errorToken(std::string_view message)const;
-	Token makeToken(TokenType type)const;
-	bool isAtEnd()const;
-	bool match(char expected);
-	bool match(char expected);
-	char peekNext()const;
-};
+	private :
+		char advance();
+		Token identifier();
+		Token number();
+		//Token string();
+		TokenType identifierType()const;
+		TokenType checkKeyword(size_t begin, size_t length, std::string_view rest, TokenType type)const;
+		Token errorToken(std::string_view message)const;
+		Token makeToken(TokenType type)const;
+		bool isAtEnd()const;
+		bool match(char expected);
+		char peekNext()const;
+		char peek()const;
+	};
+}
 
 	
 
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -2,153 +2,141 @@
 
 #include <iostream>
 
-namespace Quar {
-
-char Scanner::advance() {
-	current++;
-	return source.at(current - 1);
-}
-
-char Scanner::peek() const {
-	if (isAtEnd()) return '\0';
-	return source.at(current);
-}
-
-char Scanner::peekNext() const{
-	if (is_at_end() || current + 1 == source.length())
-		return '\0';
-	return source.at(current + 1);
-}
-
-bool Scanner::match(char expected) {
-	if (isAtEnd()) return false;
-	if (source.at(current) != expected) return false;
-	current++;
-	return true;
-}
-
-Token Scanner::error_token(std::string_view message) const {
-	return Token(TokenType::Error, message, line);
-}
-
-TokenType Scanner::check_keyword(size_t begin, size_t length, std::string_view rest, TokenType type) const {
-	if ((current - start == begin + length) && (source.substr(start + begin, length) == rest))
-		return type;
-	return TokenType::Identifier;
-}
-
-TokenType Scanner::identifierType() const {
-	switch (source.at(start))
-	{
-		case 'a': return check_keyword(1, 2, "nd", TokenType::And);
-		case 'c': return check_keyword(1, 4, "lass", TokenType::Class);
-		case 'e': return check_keyword(1, 3, "lse", TokenType::Else);
-		case 'f':
-			if (current - start > 1)
-			{
-				switch (source.at(start + 1))
-				{
-					case 'a': return check_keyword(2, 3, "lse", TokenType::False);
-					case 'o': return check_keyword(2, 1, "r", TokenType::For);
-					case 'u': return check_keyword(2, 1, "n", TokenType::Fun);
-				}
-			}
-			break;
-		case 'i': return check_keyword(1, 1, "f", TokenType::If);
-		case 'n': return check_keyword(1, 2, "il", TokenType::Nil);
-		case 'o': return check_keyword(1, 1, "r", TokenType::Or);
-		case 'p': return check_keyword(1, 4, "rint", TokenType::Print);
-		case 'r': return check_keyword(1, 5, "eturn", TokenType::Return);
-		case 's': return check_keyword(1, 4, "uper", TokenType::Super);
-		case 't':
-			if (current - start > 1)
-			{
-				switch (source.at(start + 1))
-				{
-					case 'h': return check_keyword(2, 2, "is", TokenType::This);
-					case 'r': return check_keyword(2, 2, "ue", TokenType::True);
-				}
-			}
-			break;
-		case 'v': return check_keyword(1, 2, "ar", TokenType::Var);
-		case 'w': return check_keyword(1, 4, "hile", TokenType::While);
-		default:
-			break;
+namespace quar {
+	void Token::print() {
+		if(this->type == TokenType::TOKEN_LEFT_BRACE)
+			std::cout<<"Success";
 	}
-	return TokenType::Identifier;
-}
+	Token Scanner::makeToken(TokenType type) const {
+		size_t length = current - start;
+		return Token(type, source.substr(start, length), line);
+	}
 
-Token Scanner::scan_token() {
+	bool Scanner::isAtEnd() const {
+		return current == source.length();
+	}
+
+	char Scanner::advance() {
+		current++;
+		return source.at(current - 1);
+	}
+
+	char Scanner::peek() const {
+		if (isAtEnd()) return '\0';
+		return source.at(current);
+	}
+
+	char Scanner::peekNext() const{
+		if (isAtEnd() || current + 1 == source.length())
+			return '\0';
+		return source.at(current + 1);
+	}
+
+	bool Scanner::match(char expected) {
+		if (isAtEnd()) return false;
+		if (source.at(current) != expected) return false;
+		current++;
+		return true;
+	}
+
+	Token Scanner::errorToken(std::string_view message) const {
+		return Token(TokenType::TOKEN_ERROR, message, line);
+	}
+
+	TokenType Scanner::checkKeyword(size_t begin, size_t length, std::string_view rest, TokenType type) const {
+		if ((current - start == begin + length) && (source.substr(start + begin, length) == rest))
+			return type;
+		return TokenType::TOKEN_IDENTIFIER;
+	}
+
+	TokenType Scanner::identifierType() const {
+		switch (source.at(start)) {
+			case 'a': return checkKeyword(1, 2, "nd", TokenType::TOKEN_AND);
+			//case 'c': return checkKeyword(1, 4, "lass", TokenType::Class);
+			case 'e': return checkKeyword(1, 3, "lse", TokenType::TOKEN_ELSE);
+			case 'f':
+				if (current - start > 1) {
+					switch (source.at(start + 1)) {
+						case 'a': return checkKeyword(2, 3, "lse", TokenType::TOKEN_FALSE);
+						case 'o': return checkKeyword(2, 1, "r", TokenType::TOKEN_FOR);
+						//case 'u': return checkKeyword(2, 1, "n", TokenType::Fun);
+					}
+				}
+				break;
+			case 'i': return checkKeyword(1, 1, "f", TokenType::TOKEN_IF);
+			case 'n': return checkKeyword(1, 2, "il", TokenType::TOKEN_NIL);
+			case 'o': return checkKeyword(1, 1, "r", TokenType::TOKEN_OR);
+			case 'p': return checkKeyword(1, 4, "rint", TokenType::TOKEN_PRINT);
+			case 'r': return checkKeyword(1, 5, "eturn", TokenType::TOKEN_RETURN);
+			//case 's': return checkKeyword(1, 4, "uper", TokenType::Super);
+			case 't':
+				if (current - start > 1) {
+					switch (source.at(start + 1)) {
+						case 'h': return checkKeyword(2, 2, "is", TokenType::TOKEN_THIS);
+						case 'r': return checkKeyword(2, 2, "ue", TokenType::TOKEN_TRUE);
+					}
+				}
+				break;
+			case 'v': return checkKeyword(1, 2, "ar", TokenType::TOKEN_VAR);
+			case 'w': return checkKeyword(1, 4, "hile", TokenType::TOKEN_WHILE);
+			default:
+				break;
+		}
+	return TokenType::TOKEN_IDENTIFIER;
+	}
+
+	Token Scanner::scanToken() {
 	//skip_whitespace();
 
-	start = current;
-	if (is_at_end()) return make_token(TokenType::Eof);
+		start = current;
+		if (isAtEnd()) return makeToken(TokenType::TOKEN_EOF);
 
-	auto c = advance();
-	if (std::isalpha(c)) 
-		return identifier();
-	if (std::isdigit(c)) 
-		return number();
-	switch (c) {
-		case '(': return makeToken(TokenType::LeftParent);
-		case ')': return makeToken(TokenType::RightParent);
-		case '{': return makeToken(TokenType::LeftBrace);
-		case '}': return makeToken(TokenType::RightBrace);
-		case ';': return makeToken(TokenType::Semicolon);
-		case ',': return makeToken(TokenType::Comma);
-		case '.': return makeToken(TokenType::Dot);
-		case '-': return makeToken(TokenType::Minus);
-		case '+': return makeToken(TokenType::Plus);
-		case '/': return makeToken(TokenType::Slash);
-		case '*': return makeToken(TokenType::Star);
-		case '!':
-			return makeToken(match('=') ? TokenType::BangEqual : TokenType::Bang);
-		case '=':
-			return makeToken(match('=') ? TokenType::EqualEqual : TokenType::Equal);
-		case '<':
-			return makeToken(match('=') ? TokenType::LessEqual : TokenType::Less);
-		case '>':
-			return makeToken(match('=') ? TokenType::GreaterEqual : TokenType::Greater);
-		case '"': return string();
+		auto c = advance();
+		if (std::isalpha(c)) 
+			return identifier();
+		if (std::isdigit(c))
+			return number();
+		switch (c) {
+			case '(': std::cout<<"YYY";return makeToken(TokenType::TOKEN_LEFT_PAREN);
+			case ')': return makeToken(TokenType::TOKEN_RIGHT_PAREN);
+			case '{': return makeToken(TokenType::TOKEN_LEFT_BRACE);
+			case '}': return makeToken(TokenType::TOKEN_RIGHT_BRACE);
+			case ';': return makeToken(TokenType::TOKEN_SEMICOLON);
+			case ',': return makeToken(TokenType::TOKEN_COMMA);
+			case '.': return makeToken(TokenType::TOKEN_DOT);
+			case '-': return makeToken(TokenType::TOKEN_MINUS);
+			case '+': return makeToken(TokenType::TOKEN_PLUS);
+			case '/': return makeToken(TokenType::TOKEN_SLASH);
+			case '*': return makeToken(TokenType::TOKEN_STAR);
+			case '!':
+				return makeToken(match('=') ? TokenType::TOKEN_BANG_EQUAL : TokenType::TOKEN_BANG);
+			case '=':
+				return makeToken(match('=') ? TokenType::TOKEN_EQUAL_EQUAL : TokenType::TOKEN_EQUAL);
+			case '<':
+				return makeToken(match('=') ? TokenType::TOKEN_LESS_EQUAL : TokenType::TOKEN_LESS);
+			case '>':
+				return makeToken(match('=') ? TokenType::TOKEN_GREATER_EQUAL : TokenType::TOKEN_GREATER);
+			//case '"': return string();
+		}
+		return errorToken("Unexpected character.");
 	}
-	return error_token("Unexpected character.");
-}
-
-Token Scanner::string()
-{
-	while (peek() != '"' && !isAtEnd())
-	{
-		if (peek() == '\n') line++;
-		advance();
-	}
-
-	if (isAtEnd())
-		return error_token("Unterminated string.");
-
-	advance();
-	return make_token(TokenType::String);
-}
-
-Token Scanner::number()
-{
-	while (std::isdigit(peek()))
-		advance();
-
-	if (peek() == '.' && std::isdigit(peek_next()))
-	{
-		advance();
+	
+	Token Scanner::number() {
 		while (std::isdigit(peek()))
 			advance();
+		if (peek() == '.' && std::isdigit(peekNext())) {
+			advance();
+			while (std::isdigit(peek()))
+				advance();
+		}
+		return makeToken(TokenType::TOKEN_NUMBER);
 	}
 
-	return makeToken(TokenType::Number);
-}
-
-Token Scanner::identifier()
-{
-	while (std::isalnum(peek()))
-		advance();
-	return makeToken(identifierType());
+	Token Scanner::identifier() {
+		while (std::isalnum(peek()))
+			advance();
+		return makeToken(identifierType());
+	}
 }
 
 
